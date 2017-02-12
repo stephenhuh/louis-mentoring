46999f9badde418c72ae78b12e8ffbabdff3f9ad
flyblackbox <louis@flyblackbox.com>
2017-02-11 19:14:28 -0500

    Add files via upload

---
+++ Project 1 Complete (no jQuery)/script.js
@@ -0,0 +1,27 @@
+var xhr = new XMLHttpRequest();
+
+
+function processRequest(e) {
+	
+	if (xhr.readyState == 4 && xhr.status == 200) {
+        
+        var response = JSON.parse(xhr.responseText);
+        console.log(response.msg);
+        var container = document.querySelectorAll("holder");
+        console.log(container);
+        holder.innerHTML = response.msg;
+
+
+	for(var key in response.menu) { 
+		console.log(response.menu[key]); 
+		holder.innerHTML = holder.innerHTML + "<br>" + key + ": " + response.menu[key];
+	}
+    }
+}
+
+function openRequest() {
+	xhr.open('GET', "http://demo3354820.mockable.io/menu/sushi", true);
+	xhr.send();
+}
+
+xhr.addEventListener("readystatechange", processRequest, false);
